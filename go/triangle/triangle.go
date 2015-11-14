f9a89d4971eedc6fd22894b05d0ab14b2178a482
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-11-14 14:57:27 -0500

    No need to check for negative sides since we're checking that each pair of sides is longer than the third.

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -15,7 +15,7 @@ const (
 func KindFromSides(a, b, c float64) Kind {
 	// If each two sides aren't greater than the other or any side is
 	// not greater than 0, it's not a triangle.
-	if !(a + b > c) || !(a + c > b) || !(b + c > a) || !(a > 0 && b > 0 && c > 0) { return NaT }
+	if !(a + b > c && a + c > b && b + c > a) { return NaT }
 	// If all sides are equal, it's equilateral
 	if a == b && b == c { return Equ }
 	// If two of the sides are the same, then it's isosceles
