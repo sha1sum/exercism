4170ce4266490dcc371530e1f0cd41a72846937a
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-11-14 15:30:00 -0500

    Interesting mathematical formulae for calculating sums of squares and squares of sums (this type of math is not my strong suit).

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -2,20 +2,13 @@ package diffsquares
 
 // Get the square of the sums of 1 through the number provided
 func SquareOfSums(number int) int {
-	sum := 0
-	for i := 0; i <= number; i++ {
-		sum += i
-	}
+	sum := number * (number + 1) / 2
 	return sum * sum
 }
 
 // Get the sum of the squares from 1 through the number provided
 func SumOfSquares(number int) int {
-	sum := 0
-	for i := 0; i <= number; i++ {
-		sum += i * i
-	}
-	return sum
+	return number * (number + 1) * (2 * number + 1) / 6
 }
 
 // Get the difference of the square of the sums and the sum of the squares of the number provided
