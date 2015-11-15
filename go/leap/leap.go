6ce7b40d1529f114401131a50983c59964571914
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-11-15 13:30:31 -0500

    Formatted code.

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -4,5 +4,5 @@ const TestVersion = 1
 
 // Take a year argument and return whether that year is a leap year or not.
 func IsLeapYear(year int) bool {
-	return year % 4 == 0 && !(year % 100 == 0 && !(year % 400 == 0))
+	return year%4 == 0 && !(year%100 == 0 && !(year%400 == 0))
 }
