3435f67e2b55b5e56bc68b3f0b6c58644c47d4ef
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-11-15 13:29:41 -0500

    Simplified leap year formula.

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -4,14 +4,5 @@ const TestVersion = 1
 
 // Take a year argument and return whether that year is a leap year or not.
 func IsLeapYear(year int) bool {
-	// If the year is not divisible by 4, don't bother proceeding.
-	if year % 4 != 0 { return false }
-	switch {
-	// If the year is divisible by 400, it's a leap year.
-	case year % 400 == 0: return true
-	// When the year is divisible by 100, but not by 400, it's not a leap year.
-	case year % 100 == 0: return false
-	// Every year that does not meet the first two cases is a leap year.
-	default: return true
-	}
+	return year % 4 == 0 && !(year % 100 == 0 && !(year % 400 == 0))
 }
