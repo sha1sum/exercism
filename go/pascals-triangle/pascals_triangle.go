0f2262eaa65f692dbe8fd43d38404e19a66fce09
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-11-15 14:52:32 -0500

    More efficient Pascal's triangle.

--- go/pascals-triangle/pascals_triangle.go
+++ go/pascals-triangle/pascals_triangle.go
@@ -1,48 +1,26 @@
 package pascal
 
-// Build Pascal's triangle. Formula: row! / (column! * (row - column)!)
+// Build Pascal's triangle to number of specified rows.
 func Triangle(rows int) [][]int {
-	triangle := make([][]int, 0)
-	// Return an empty slice if no rows
-	if rows < 1 {
+	// Initialize the triangle with the first row being 1
+	triangle := [][]int{[]int{1}}
+	// If there's only one row, just return it
+	if rows <= 1 {
 		return triangle
 	}
-	// The first row is always just 1
-	triangle = append(triangle, []int{1})
-	// For each row after the first
-	for row := 1; row < rows; row++ {
-		rowValues := make([]int, 0)
-		// Calculate all of the descending natural numbers from the row number multiplied together
-		rowDescendant := row
-		for descendant := row - 1; descendant > 0; descendant-- {
-			rowDescendant *= descendant
+	// Loop through the number of rows, getting the previous row index
+	for previousRow := 0; previousRow < rows-1; previousRow++ {
+		// The first number in the row is always 1
+		row := []int{1}
+		// Loop through all the columns after the first and up to the last
+		for i := 1; i < len(triangle[previousRow]); i++ {
+			// Add the value of the column before the current one and the current column of the previous row and append
+			row = append(row, triangle[previousRow][i-1]+triangle[previousRow][i])
 		}
-		// For each column in the row
-		for column := 0; column <= row; column++ {
-			// First and last columns are always 1
-			if column == 0 || column == row {
-				rowValues = append(rowValues, 1)
-				continue
-			}
-			// Calculate all of the descending natural numbers from the column number multiplied together
-			columnDescendant := column
-			for descendant := column - 1; descendant > 0; descendant-- {
-				columnDescendant *= descendant
-			}
-			// Calculate all of the descending natural numbers from the difference between the
-			// row and column numbers multiplied together
-			rowMinusColumn := row - column
-			rowColDescendant := rowMinusColumn
-			for descendant := rowMinusColumn - 1; descendant > 0; descendant-- {
-				rowColDescendant *= descendant
-			}
-			// Put all of the descendant multiplication values together into the final formula
-			columnValue := rowDescendant / (columnDescendant * rowColDescendant)
-			// Append the column value to the row
-			rowValues = append(rowValues, columnValue)
-		}
-		// Append the row values to the triangle
-		triangle = append(triangle, rowValues)
+		// The last number in the row is always 1
+		row = append(row, 1)
+		// Append the row to the triangle
+		triangle = append(triangle, row)
 	}
 	return triangle
 }
